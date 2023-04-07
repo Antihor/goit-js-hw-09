7432091169e716cc5d2d4f48173c4aa2c43ab2c1
Antihor <66antigor@gmail.com>
2023-04-07 11:13:16 +0300

    aaa

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
-bbbbbbbb;
+const startRef = document.querySelector('button[data-start]');
+const stopRef = document.querySelector('button[data-stop]');
+const bodyRef = document.querySelector('body');
+
+let timeoutId = null;
+
+startRef.addEventListener('click', onStart);
+stopRef.addEventListener('click', onStop);
+
+function onStart() {
+  timerId = setInterval(() => {
+    bodyRef.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  startRef.disabled = true;
+  stopRef.disabled = false;
+}
+function onStop() {
+  clearInterval(timerId);
+  startRef.disabled = false;
+  stopRef.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
